f534d0397a304c671aacfa3d67ca5a4a106c58da
Gatu Cristian <ocu01811@osramcontinental.net>
2020-05-24 13:15:15 +0300

    Fix server bug

--- servar/test.py
+++ servar/test.py
@@ -1,23 +1,23 @@
-from functions import from_text_to_location,generate_answer,create_mp3_from_text,get_time
+from functions import from_text_to_location, generate_answer, create_mp3_from_text, get_time
 import psycopg2
 import sys
 import os
-create_mp3_from_text("Unde am fost acum 9 zile ","./Uploads","test_final")
+create_mp3_from_text("Unde am fost acum 10 zile ", "./Uploads", "test_final")
 
 con = psycopg2.connect(user="postgres",
-                           password="parola",
-                           host="127.0.0.1",
-                           port="5432",
-                           database="tilndb")
+                       password="parola",
+                       host="127.0.0.1",
+                       port="5432",
+                       database="tilndb")
 cursor = con.cursor()
 # os.system("start ./Uploads/test.mp3")
 # raw=get_data_from_database(cursor,'2020-05-23',1)
 # # print(raw)
-text="acum 9 zile"
+text = "acum 9 zile"
 # date,time=get_time(text)
 # print(date,time)
 # h=from_text_to_location(1,text,cursor)
-h=generate_answer("./Uploads","test_final",1,cursor)
+h = generate_answer("./Uploads", "test_final", 1, cursor)
 print(h)
 # text.replace('o saptamana','1 saptamana')
 # print(text)
